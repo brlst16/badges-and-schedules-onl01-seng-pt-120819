e5cffcff3d9a489f20fe682a2ce33d11e9872745
Brigette LeVert <bnl.levert@gmail.com>
2019-12-21 17:20:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+ def batch_badge_creator(array_of_names)
+   array_of_names.collect { |name| "Hello, my name is #{name}."}
+ end
+
+# def assign_rooms(speakers)
+#   speakers.each_with_index.collect  do |name, index| 
+#     puts "Hello, #{name}! You'll be assigned to room #{index+1}!"
+# end
+
+# def assign_rooms(speakers)
+# #     speakers.collect.with_index(1) do |speaker, index|
+# #     "Hello, #{speaker}! You'll be assigned to room #{index}!"
+# # end
+
+
+# def printer(speakers)
+#   batch_badge_creator(speakers).each { |element| puts element}
+#   assign_rooms(speakers).each { |element| puts element}
+# end 
